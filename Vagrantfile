eb3c79186d56ed18325eb68fe7f4074eb0bde6c3
Chris Palmeri <chrispalmeri@gmail.com>
2019-11-23 20:15:05 -0600

    Separate dist files into directory

--- Vagrantfile
+++ Vagrantfile
@@ -4,5 +4,6 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "bento/ubuntu-18.04"
   config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+  config.vm.synced_folder "./dist", "/var/www/html"
   config.vm.provision "shell", path: "./provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -4,24 +4,3 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get install -y apache2
-
-mkdir -p /vagrant/.logs
-
-cat > /etc/apache2/sites-available/000-default.conf << EOF
-<VirtualHost *:80>
-    DocumentRoot /vagrant
-
-    <Directory /vagrant>
-        Options -Indexes
-        AllowOverride All
-        Require all granted
-        Header set Access-Control-Allow-Origin "*"
-    </Directory>
-
-    ErrorLog /vagrant/.logs/apache-error.log
-    CustomLog /vagrant/.logs/apache-access.log combined
-</VirtualHost>
-EOF
-
-a2enmod headers
-systemctl restart apache2
